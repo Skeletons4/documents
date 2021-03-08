a8ecf397b008cde5da7b0f94e50f8c978e010447
sunguobin <Skleltons430@163.com>
2021-03-08 14:21:43 +0800

    更新配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,26 @@ module.exports = {
     title: '文档中心',
     plugins: {
         "vuepress-plugin-auto-sidebar": {}
+    },
+    themeConfig: {
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'Skeletons4/documents',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: '查看源码',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        docsRepo: 'Skeletons4/documents',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'master',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: '帮助我们改善此页面！',
+        smoothScroll: true
     }
 }
